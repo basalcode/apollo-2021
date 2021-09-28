73faf488eb4091a5a475c451ee9c50a789ca2cc1
basalcode <basalcode@gmail.com>
2021-09-28 15:30:10 +0900

    Implement CSS and get data from api

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,74 @@
-export default () => "Detail";
+import { useParams } from 'react-router-dom';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/client';
+import styled from 'styled-components';
+
+
+const GET_MOVIE = gql`
+    query getMovie($id: Int!) {
+        movie(id: $id) {
+            title
+            medium_cover_image
+            language
+            rating
+            description_intro
+        }
+    }
+`;
+
+const Container = styled.div`
+    height: 100vh;
+    width: 100%;
+    background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    color: white;
+`;
+
+const Column = styled.div`
+    margin-left: 10px;
+`;
+
+const Title = styled.h1`
+    font-size: 65px;
+    margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+    font-size: 35px;
+    margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+    font-size: 28px;
+
+`;
+
+const Poster = styled.div`
+    width: 25%;
+    height: 60%;
+    background-color: transparent;
+`;
+
+const Detail = () => {
+    const { id } = useParams();
+    const { loading, data } = useQuery(GET_MOVIE, {
+        variables: {
+            id: Number(id)
+        }
+    });
+
+    return (
+        <Container>
+            <Column>
+                <Title>Name</Title>
+                <Subtitle>English · 4.5</Subtitle>
+                <Description>lorem ipsum lalalala</Description>
+            </Column>
+            {!loading && <Poster></Poster>}
+        </Container>
+    );
+}
+
+export default Detail;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,81 @@
-export default () => "Home";
+import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
+import Movie from '../components/Movie';
+import styled from 'styled-components';
+
+const GET_MOVIES = gql`
+    {
+        movies {
+            id
+            medium_cover_image
+        }
+    }
+`;
+
+const Container =styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+`;
+
+const Header =styled.div`
+    background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+    height: 45vh;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+`;
+
+const Title =styled.div`
+    font-size: 60px;
+    font-weight: 600;
+    margin-bottom: 20px;
+`;
+
+const Subtitle =styled.div`
+    font-size: 35px;
+`;
+
+const Loading = styled.div`
+    font-size: 18px;
+    opacity: 0.5;
+    font-weight: 500;
+    margin-top: 10px;
+`;
+
+const Movies = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-gap: 25px;
+    width: 60%;
+    position: relative;
+    top: -50px;
+`;
+
+const Home = () => {
+    const { loading, error, data } = useQuery(GET_MOVIES);
+    console.log('loading', loading, error, data);
+
+    return (
+        <Container>
+            <Header>
+                <Title>Apollo 2020</Title>
+                <Subtitle>I love Graph</Subtitle>
+            </Header>
+            {loading && <Loading>Loading...</Loading>}
+            {!loading && data.movies &&
+                <Movies>
+                    {data.movies.map(m => <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />)}
+                </Movies>
+            }
+        </Container>
+    );
+};
+
+export default Home;
+
